03d29b3559549c0692a63b9c926557a681be72a9
Welisson Wilson Oliveira <welisson.oliveira@sonda.com>
2021-07-12 22:47:57 -0300

    configuração do hystrix e adição do application.yml da loja

--- loja/src/main/java/br/com/alura/microservice/loja/domain/purchase/PurchaseApplicationService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/domain/purchase/PurchaseApplicationService.java
@@ -3,6 +3,7 @@ package br.com.alura.microservice.loja.domain.purchase;
 import br.com.alura.microservice.loja.controller.dto.BookingItemDTO;
 import br.com.alura.microservice.loja.controller.dto.SupplierDTO;
 import br.com.alura.microservice.loja.infrasctructure.feign.SupplierClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,10 +19,26 @@ public class PurchaseApplicationService {
     @Value("${supplier-url}")
     private String url;
 
+    @HystrixCommand(fallbackMethod = "makePurchaseFallback")
     public Purchase makePurchase(Purchase purchase) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         log.info("Buscando informações de fornecedor {}", purchase.getState());
         SupplierDTO supplier = supplierClient.getSupplierByState(purchase.getState());
         log.info("Realizando um pedido");
+        BookingItemDTO order = supplierClient.makeOrder(supplier.getId(), purchase.getItems());
+
+        return new Purchase(order.getId(), order.getPreparationTime(), purchase.getAddress(), purchase.getItems());
+    }
+
+    public Purchase makePurchaseFallback(Purchase purchase){
+        log.info("FallBack: Buscando informações de fornecedor {}", purchase.getState());
+        SupplierDTO supplier = supplierClient.getSupplierByState(purchase.getState());
+        log.info("FallBack:  Realizando um pedido");
         BookingItemDTO order = supplierClient.makeOrder(supplier.getId(), purchase.getItems());
 
         return new Purchase(order.getId(), order.getPreparationTime(), purchase.getAddress(), purchase.getItems());
